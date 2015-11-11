384d9ee10baa42bf2d2dce1ba622fc869337f8f8
a022599176c <a022599176c@s09-pc06>
2015-11-11 20:03:29 +0100

    Inicio

---
+++ src/holamundogit/HolaMundoGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package holamundogit;
+
+/**
+ *
+ * @author a022599176c
+ */
+public class HolaMundoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hola Mundo ");
+        System.out.println("Rojo");
+    }
+    
+}
